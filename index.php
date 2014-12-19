b72f4e10718e110b3cf0118f9028242becd505bb
hooman <hooman@mirghasemi.com>
2014-12-19 11:42:43 +0330

    changing title of index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>This is</title>
+        <title>This is good practis for learning git and using it</title>
     </head>
     <body>
         <?php
